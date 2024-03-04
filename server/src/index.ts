f3944bc132cd8e94b6cf6b78be1fe089bea66898
k_95.21 <kohta0421kochi@gmail.com>
2024-03-04 10:25:01 +0900

    add prisma

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, world!");
+import express from "express";
+import type { Express, Request, Response } from "express";
+
+const app: Express = express();
+const PORT = 8080;
+
+app.get("/allTodos", (req: Request, res: Response) => {
+    return res.send("Todos")
+})
+
+app.listen(PORT, () => console.log("Server is running on port 8080"));
